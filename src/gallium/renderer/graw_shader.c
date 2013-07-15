378b43bff691c6df24b9d0d2fe3220f5501e862b
Dave Airlie <airlied@redhat.com>
2013-07-15 16:55:29 +1000

    fix negation of immediates

--- src/gallium/renderer/graw_shader.c
+++ src/gallium/renderer/graw_shader.c
@@ -317,13 +317,13 @@ iter_instruction(struct tgsi_iterate_context *iter,
                idx = src->Register.SwizzleW;
             switch (imd->type) {
             case TGSI_IMM_FLOAT32:
-               snprintf(temp, 25, "%.4f", imd->val[idx].f);
+               snprintf(temp, 25, "%c%.4f", negate, imd->val[idx].f);
                break;
             case TGSI_IMM_UINT32:
-               snprintf(temp, 25, "%u", imd->val[idx].ui);
+               snprintf(temp, 25, "%c%u", negate, imd->val[idx].ui);
                break;
             case TGSI_IMM_INT32:
-               snprintf(temp, 25, "%d", imd->val[idx].i);
+               snprintf(temp, 25, "%c%d", negate, imd->val[idx].i);
                break;
             }
             strncat(srcs[i], temp, 255);

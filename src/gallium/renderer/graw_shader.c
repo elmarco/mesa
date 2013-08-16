b7afcd18ae419ed025563f3f5fdd190ef9d95f6f
Dave Airlie <airlied@gmail.com>
2013-08-16 11:40:19 +1000

    emit arith op2 in a macro

--- src/gallium/renderer/graw_shader.c
+++ src/gallium/renderer/graw_shader.c
@@ -259,6 +259,7 @@ static void emit_so_movs(struct dump_ctx *ctx)
 
 }
 
+#define emit_arit_op2(op) snprintf(buf, 255, "%s = %s(%s %s %s);\n", dsts[0], dstconv, srcs[0], op, srcs[1])
 #define emit_op1(op) snprintf(buf, 255, "%s = %s(%s(%s));\n", dsts[0], dstconv, op, srcs[0])
 #define emit_compare(op) snprintf(buf, 255, "%s = %s((%s(%s, %s))%s);\n", dsts[0], dstconv, op, srcs[0], srcs[1], writemask)
 static boolean
@@ -512,20 +513,36 @@ iter_instruction(struct tgsi_iterate_context *iter,
       strcat(ctx->glsl_main, buf);
       break;
    case TGSI_OPCODE_ADD:
-      snprintf(buf, 255, "%s = %s(%s + %s);\n", dsts[0], dstconv, srcs[0], srcs[1]);
+      emit_arit_op2("+");
       strcat(ctx->glsl_main, buf);
       break;
    case TGSI_OPCODE_SUB:
-      snprintf(buf, 255, "%s = %s(%s - %s);\n", dsts[0], dstconv, srcs[0], srcs[1]);
+      emit_arit_op2("-");
       strcat(ctx->glsl_main, buf);
       break;
    case TGSI_OPCODE_MUL:
    case TGSI_OPCODE_UMUL:
-      snprintf(buf, 255, "%s = %s(%s * %s);\n", dsts[0], dstconv, srcs[0], srcs[1]);
+      emit_arit_op2("*");
       strcat(ctx->glsl_main, buf);
       break;
    case TGSI_OPCODE_MAD:
       snprintf(buf, 255, "%s = %s(%s * %s + %s);\n", dsts[0], dstconv, srcs[0], srcs[1], srcs[2]);
+      strcat(ctx->glsl_main, buf);
+      break;
+   case TGSI_OPCODE_OR:
+      emit_arit_op2("|");
+      strcat(ctx->glsl_main, buf);
+      break;
+   case TGSI_OPCODE_AND:
+      emit_arit_op2("&");
+      strcat(ctx->glsl_main, buf);
+      break;
+   case TGSI_OPCODE_XOR:
+      emit_arit_op2("^");
+      strcat(ctx->glsl_main, buf);
+      break;
+   case TGSI_OPCODE_MOD:
+      emit_arit_op2("%");
       strcat(ctx->glsl_main, buf);
       break;
    case TGSI_OPCODE_TEX:

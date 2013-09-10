4c73e87b95f3dc3bcc7178e7dedbfc091783c1b7
Dave Airlie <airlied@gmail.com>
2013-09-10 14:47:36 +1000

    disable zcomp

--- src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
+++ src/gallium/drivers/nouveau/nvc0/nvc0_screen.c
@@ -728,7 +728,8 @@ nvc0_screen_create(struct nouveau_device *dev)
       PUSH_DATA (push, 0x17);
    }
 
-   IMMED_NVC0(push, NVC0_3D(ZETA_COMP_ENABLE), dev->drm_version >= 0x01000101);
+   IMMED_NVC0(push, NVC0_3D(ZETA_COMP_ENABLE), 0);
+ //dev->drm_version >= 0x01000101);
    BEGIN_NVC0(push, NVC0_3D(RT_COMP_ENABLE(0)), 8);
    for (i = 0; i < 8; ++i)
            PUSH_DATA(push, dev->drm_version >= 0x01000101);

b380e6a670004f48f861765681872ffd31a7623b
Dave Airlie <airlied@gmail.com>
2014-03-27 12:30:11 +1000

    renderer: drop lots of old unused code

--- src/gallium/renderer/graw_renderer_glx.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef GRAW_RENDERER_GLX
-#define GRAW_RENDERER_GLX
-void graw_renderer_init_glx(int localrender);
-void graw_renderer_fini_glx(void);
-int process_x_event(void);
-int swap_buffers(void);
-
-#endif

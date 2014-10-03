544852d47ca02652a62f65d7502e6354b1ae498e
Dave Airlie <airlied@redhat.com>
2014-10-03 16:18:40 +1000

    renderer: handle PBO's better

--- src/gallium/renderer/vrend_renderer.c
+++ src/gallium/renderer/vrend_renderer.c
@@ -3138,17 +3138,19 @@ void vrend_renderer_resource_create(struct vrend_renderer_resource_create_args *
       glGenBuffersARB(1, &gr->id);
       glBindBufferARB(gr->target, gr->id);
       glBufferData(gr->target, args->width, NULL, GL_STREAM_DRAW);
-   } else if (args->target == PIPE_BUFFER && args->bind == PIPE_BIND_SAMPLER_VIEW) {
+   } else if (args->target == PIPE_BUFFER && (args->bind & PIPE_BIND_SAMPLER_VIEW)) {
       GLenum internalformat;
+      /* need to check GL version here */
       gr->target = GL_TEXTURE_BUFFER;
       glGenBuffersARB(1, &gr->id);
       glBindBufferARB(gr->target, gr->id);
       glGenTextures(1, &gr->tbo_tex_id);
-      glBindTexture(gr->target, gr->tbo_tex_id);
+      glBufferData(gr->target, args->width, NULL, GL_STREAM_DRAW);
 
+      glBindTexture(gr->target, gr->tbo_tex_id);
       internalformat = tex_conv_table[args->format].internalformat;
       glTexBuffer(gr->target, internalformat, gr->id);
-      glBufferData(gr->target, args->width, NULL, GL_STREAM_DRAW);
+
    } else {
       struct vrend_texture *gt = (struct vrend_texture *)gr;
       GLenum internalformat, glformat, gltype;

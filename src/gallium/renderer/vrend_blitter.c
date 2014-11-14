b073054e575cb82cc1086fb6647efd861c1f52b8
Dave Airlie <airlied@redhat.com>
2014-11-14 16:21:18 +1000

    blitter: add cube map support

--- src/gallium/renderer/vrend_blitter.c
+++ src/gallium/renderer/vrend_blitter.c
@@ -39,6 +39,7 @@
 
 #include "util/u_double_list.h"
 #include "util/u_format.h"
+#include "util/u_texture.h"
 #include "tgsi/tgsi_parse.h"
 
 #include "vrend_object.h"
@@ -381,10 +382,21 @@ static void blitter_set_texcoords(struct vrend_blitter_ctx *blit_ctx,
                                   int x1, int y1, int x2, int y2)
 {
    float coord[4];
+   float face_coord[4][2];
    int i;
    get_texcoords(src_res, level, x1, y1, x2, y2, coord);
 
-   set_texcoords_in_vertices(coord, &blit_ctx->vertices[0][1][0], 8);
+   if (src_res->base.target == PIPE_TEXTURE_CUBE ||
+       src_res->base.target == PIPE_TEXTURE_CUBE_ARRAY) {
+      set_texcoords_in_vertices(coord, &face_coord[0][0], 2);
+      util_map_texcoords2d_onto_cubemap((unsigned)layer % 6,
+                                        /* pointer, stride in floats */
+                                        &face_coord[0][0], 2,
+                                        &blit_ctx->vertices[0][1][0], 8,
+                                        FALSE);
+   } else {
+      set_texcoords_in_vertices(coord, &blit_ctx->vertices[0][1][0], 8);
+   }
 
    switch (src_res->base.target) {
    case PIPE_TEXTURE_3D:
@@ -394,6 +406,10 @@ static void blitter_set_texcoords(struct vrend_blitter_ctx *blit_ctx,
          for (i = 0; i < 4; i++)
             blit_ctx->vertices[i][1][2] = r; /*r*/
       }
+      break;
+   case PIPE_TEXTURE_CUBE_ARRAY:
+      for (i = 0; i < 4; i++)
+         blit_ctx->vertices[i][1][3] = (float) ((unsigned)layer / 6); /*w*/
       break;
    case PIPE_TEXTURE_2D:
        for (i = 0; i < 4; i++) {
